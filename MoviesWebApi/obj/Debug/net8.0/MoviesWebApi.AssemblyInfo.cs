43abd5c4fa8dddca44263923f993f3a15b275018
Angel <avegasam@gmail.com>
2024-03-03 22:58:47 -0500

    Add README

--- MoviesWebApi/obj/Debug/net8.0/MoviesWebApi.AssemblyInfo.cs
+++ MoviesWebApi/obj/Debug/net8.0/MoviesWebApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MoviesWebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e7b19ba65313cf4808d608433833e9bafce2aec0")]
 [assembly: System.Reflection.AssemblyProductAttribute("MoviesWebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MoviesWebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ README.md
@@ -0,0 +1,19 @@
+# Movies App
+
+Aplicación web que permita ver la cartelera de películas disponibles en las salas
+de cine.
+
+## Tabla de Contenidos
+
+1. [Instalación](#instalación)
+2. [Uso](#uso)
+3. [Estructura del Proyecto](#estructura-del-proyecto)
+4. [Ejemplos de Prueba](#ejemplos-de-prueba)
+5. [Contribuciones](#contribuciones)
+6. [Licencia](#licencia)
+
+## Instalación
+
+
+```bash
+npm install
